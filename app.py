ba889bb13a451f8415b0fb387961154f4cbabe5a
yockayz <erickmartins2.5@hotmail.com>
2025-06-06 14:09:20 -0300

    gráfico atualizado

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("MS_Financial Sample.csv", on_bad_lines='skip')
+df = pd.read_csv("MS_Financial Sample.csv", sep=";", on_bad_lines="skip")
 
-st.write("Prova SOP - Erick Cardoso Martins")
-st.dataframe(df)
+# Limpeza de nomes de colunas para remover espaços extras
+df.columns = df.columns.str.strip()
+
+segment_counts = df["Segment"].value_counts().reset_index()
+segment_counts.columns = ["Segment", "Count"]
+
+top_5_segments = segment_counts.head(5)
+
+st.title("Os 5 Segmentos Mais Frequentes no Dataset")
+
+st.write("### Tabela dos 5 Segmentos Mais Frequentes")
+st.dataframe(top_5_segments)
+
+st.write("### Gráfico dos 5 Segmentos Mais Frequentes")
+st.bar_chart(top_5_segments.set_index("Segment"))
